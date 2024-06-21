d02547bfc9c2f18b571c052511a3ae998496b900
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-06-21 23:51:56 +0300

    add test for pixel char generatioin

--- src/main.c
+++ src/main.c
@@ -9,44 +9,26 @@
 #include <exti.h>      //EXTERNAL INTERUPS
 #include <sipo_sreg.h> //SERIAL IN PARRALEL OUT SHIFT REGISTER
 #include <lcd_char_disp.h>
+#include <particle_char_generator.h>
+
 int main()
 {
     // Setup
-    // gpio_init();
-    // exti_init();
-    // shift_register_init();
-
-    char output;
-    char *string = "do re mi fa sol extra rinda sol";
-    int index = 0;
     lcd_char_disp_init();
+
     // Loop
     while (true)
     {
-        // clear_display();
-        // return_home();
-        // test
-        if (index == 0)
+        // TEST
+        for (int i = 0; i < CHAR_HEIGTH; i++)
         {
-            output_char_stream(string);
-        }
-        index = 1;
-
-        for (int i = 0; i < 16; i++)
-        {
-            shift_display(true);
-            for (volatile int i = 0; i < 350000; i++)
-                ;
-        }
-        for (int i = 0; i < 8; i++)
-        {
-            shift_display(true);
-        }
-        for (int i = 0; i < 16; i++)
-        {
-            shift_display(true);
-            for (volatile int i = 0; i < 350000; i++)
-                ;
+            for (size_t j = 0; j < CHAR_WIDTH; j++)
+            {
+                generate_single_pixel_char(i, j);
+                for (volatile int i = 0; i < 100000; i++)
+                    ;
+                output_char(&(uint8_t){0x00}, 31);
+            }
         }
     }
     return 0;
