51f7ec0b302770ef640ffd04998991337a64101a
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-06-24 20:45:34 +0300

    Test program

--- src/main.c
+++ src/main.c
@@ -10,24 +10,31 @@
 #include <sipo_sreg.h> //SERIAL IN PARRALEL OUT SHIFT REGISTER
 #include <lcd_char_disp.h>
 #include <particle_char_generator.h>
+#include <physics.h>
 
 int main()
 {
     // Setup
     lcd_char_disp_init();
-
+    struct particle p1;
+    
     // Loop
     while (true)
     {
         // TEST
-        for (int i = 0; i < CHAR_HEIGTH; i++)
+        for (int i = 0; i < 16; i++) // height
         {
-            for (size_t j = 0; j < CHAR_WIDTH; j++)
+            for (int j = 0; j < 80; j++) // width
             {
-                generate_single_pixel_char(i, j);
-                for (volatile int i = 0; i < 100000; i++)
+                // for (volatile int i = 0; i < 50000; i++)
+                //     ;
+                p1.pos.x = j;
+                p1.pos.y = i;
+                // Loop
+                spawn_particle(&p1);
+                for (volatile int i = 0; i < 30000; i++) // If delay is smaller then this, LCD doesnt reach its maximum contrast, hard to see the particle.
                     ;
-                output_char(&(uint8_t){0x00}, 31);
+                clear_display();
             }
         }
     }
