7bc46db0e2e91121bf2810ed2eff0eb09c5b0e3b
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-07-02 21:52:03 +0300

    include positions.h

--- include/physics.h
+++ include/physics.h
@@ -4,19 +4,16 @@
 #include <stdint.h>
 #include <lcd_char_disp.h>
 #include <particle_char_generator.h>
+#include <positions.h>
 
-//Position 0;0 is LCD top left corner. x+ axis is right direction; y+ axis is up direction;
-struct position
-{
-    int8_t x; // 0 - 79
-    int8_t y; // 0 - 17
-};
+// Position 0;0 is LCD top left corner. x+ axis is right direction; y+ axis is up direction;
+
 struct velocity
 {
     int8_t x;
     int8_t y;
 };
-//Can be negative
+// Can be negative
 struct acceleration
 {
     int8_t x;
@@ -28,24 +25,13 @@ struct particle
     struct velocity vel;
     struct acceleration acc;
 };
-struct relative_position
-{
-    struct position char_position;  // 0-15
-    struct position pixel_position; // 0-39
-    uint8_t char_seq; // 0-31
-};
 
-// Only supports single particles
-// TODO: Add multiple particles
 static struct particle *particle_array = NULL;
 static uint32_t particle_count = 0;
 
-void spawn_particle(struct particle *particle);
 void add_particle(struct particle *particle);
-void spawn_all_particles();
 void update_particle(struct particle *particle);
 void update_all_particles();
-void display_single_particle(struct particle *particle);
 void display_all_particles();
 void free_all_particles();
 
