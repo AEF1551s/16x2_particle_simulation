c3f0f97f6431d3c0d26876a9f30a04f223592a16
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-07-02 22:01:03 +0300

    add test

--- src/main.c
+++ src/main.c
@@ -16,25 +16,63 @@ int main()
 {
     // Setup
     lcd_char_disp_init();
-    struct particle p1;
-    p1.pos.x = 40;
-    p1.pos.y = 15;
-    p1.acc.x = 0;
-    p1.acc.y = 0;
-    p1.vel.x = 2;
-    p1.vel.y = 8;
-    spawn_particle(&p1);
+    clear_display();
+
+struct particle p1;
+p1.pos.x = 40;
+p1.pos.y = 2;
+p1.acc.x = 0;
+p1.acc.y = 0;
+p1.vel.x = 2;
+p1.vel.y = 8;
+add_particle(&p1);
+
+struct particle p2;
+p2.pos.x = 41;
+p2.pos.y = 2;
+p2.acc.x = 0;
+p2.acc.y = 0;
+p2.vel.x = 2;
+p2.vel.y = 8;
+add_particle(&p2);
+
+struct particle p3;
+p3.pos.x = 42;
+p3.pos.y = 2;
+p3.acc.x = 0;
+p3.acc.y = 0;
+p3.vel.x = -1;
+p3.vel.y = 4;
+add_particle(&p3);
+
+struct particle p4;
+p4.pos.x = 43;
+p4.pos.y = 2;
+p4.acc.x = 0;
+p4.acc.y = 0;
+p4.vel.x = -4;
+p4.vel.y = 8;
+add_particle(&p4);
+
+struct particle p5;
+p5.pos.x = 0;
+p5.pos.y = 0;
+p5.acc.x = 0;
+p5.acc.y = 0;
+p5.vel.x = 0;
+p5.vel.y = 0;
+add_particle(&p5);
+
     // Loop
     while (true)
     {
-        // TEST
-        update_particle(&p1);
-
-        for (volatile int i = 0; i < 100000; i++) // If delay is smaller then 30000, LCD doesnt reach its maximum contrast, hard to see the particle.
+        display_all_particles();
+        for (volatile int i = 0; i < 50000; i++) // If delay is smaller then 30000, LCD doesnt reach its maximum contrast, hard to see the particle.
             ;
-
         clear_display();
+        update_all_particles();
     }
+    free_all_particles();
     return 0;
 }
 
