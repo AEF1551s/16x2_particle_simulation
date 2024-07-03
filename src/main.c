914bec336dca5af9a6c49b62fb3440e2348da755
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-07-03 21:30:52 +0300

    test program

--- src/main.c
+++ src/main.c
@@ -18,50 +18,50 @@ int main()
     lcd_char_disp_init();
     clear_display();
 
-struct particle p1;
-p1.pos.x = 40;
-p1.pos.y = 2;
-p1.acc.x = 0;
-p1.acc.y = 0;
-p1.vel.x = 2;
-p1.vel.y = 8;
-add_particle(&p1);
+    struct particle p1;
+    p1.pos.x = 0;
+    p1.pos.y = 1;
+    p1.acc.x = 0;
+    p1.acc.y = 0;
+    p1.vel.x = 1;
+    p1.vel.y = 0;
+    add_particle(&p1);
 
-struct particle p2;
-p2.pos.x = 41;
-p2.pos.y = 2;
-p2.acc.x = 0;
-p2.acc.y = 0;
-p2.vel.x = 2;
-p2.vel.y = 8;
-add_particle(&p2);
+    struct particle p2;
+    p2.pos.x = 0;
+    p2.pos.y = 2;
+    p2.acc.x = 0;
+    p2.acc.y = 0;
+    p2.vel.x = 2;
+    p2.vel.y = 0;
+    add_particle(&p2);
 
-struct particle p3;
-p3.pos.x = 42;
-p3.pos.y = 2;
-p3.acc.x = 0;
-p3.acc.y = 0;
-p3.vel.x = -1;
-p3.vel.y = 4;
-add_particle(&p3);
+    struct particle p3;
+    p3.pos.x = 0;
+    p3.pos.y = 3;
+    p3.acc.x = 0;
+    p3.acc.y = 0;
+    p3.vel.x = 3;
+    p3.vel.y = 0;
+    add_particle(&p3);
 
-struct particle p4;
-p4.pos.x = 43;
-p4.pos.y = 2;
-p4.acc.x = 0;
-p4.acc.y = 0;
-p4.vel.x = -4;
-p4.vel.y = 8;
-add_particle(&p4);
+    struct particle p4;
+    p4.pos.x = 0;
+    p4.pos.y = 4;
+    p4.acc.x = 0;
+    p4.acc.y = 0;
+    p4.vel.x = 4;
+    p4.vel.y = 0;
+    add_particle(&p4);
 
-struct particle p5;
-p5.pos.x = 0;
-p5.pos.y = 0;
-p5.acc.x = 0;
-p5.acc.y = 0;
-p5.vel.x = 0;
-p5.vel.y = 0;
-add_particle(&p5);
+    struct particle p5;
+    p5.pos.x = 79;
+    p5.pos.y = 7;
+    p5.acc.x = 0;
+    p5.acc.y = 0;
+    p5.vel.x = -5;
+    p5.vel.y = 0;
+    add_particle(&p5);
 
     // Loop
     while (true)
