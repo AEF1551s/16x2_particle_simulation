73eae1732a260dd65986651f2c1022fcaaa0c91a
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-07-04 12:48:56 +0300

    test program

--- src/main.c
+++ src/main.c
@@ -18,50 +18,12 @@ int main()
     lcd_char_disp_init();
     clear_display();
 
-    struct particle p1;
-    p1.pos.x = 0;
-    p1.pos.y = 1;
-    p1.acc.x = 0;
-    p1.acc.y = 0;
-    p1.vel.x = 1;
-    p1.vel.y = 0;
-    add_particle(&p1);
-
-    struct particle p2;
-    p2.pos.x = 0;
-    p2.pos.y = 2;
-    p2.acc.x = 0;
-    p2.acc.y = 0;
-    p2.vel.x = 2;
-    p2.vel.y = 0;
-    add_particle(&p2);
-
-    struct particle p3;
-    p3.pos.x = 0;
-    p3.pos.y = 3;
-    p3.acc.x = 0;
-    p3.acc.y = 0;
-    p3.vel.x = 3;
-    p3.vel.y = 0;
-    add_particle(&p3);
-
-    struct particle p4;
-    p4.pos.x = 0;
-    p4.pos.y = 4;
-    p4.acc.x = 0;
-    p4.acc.y = 0;
-    p4.vel.x = 4;
-    p4.vel.y = 0;
-    add_particle(&p4);
-
-    struct particle p5;
-    p5.pos.x = 79;
-    p5.pos.y = 7;
-    p5.acc.x = 0;
-    p5.acc.y = 0;
-    p5.vel.x = -5;
-    p5.vel.y = 0;
-    add_particle(&p5);
+    struct particle particles[5];
+    for (int i = 0; i < 5; i++)
+    {
+        randomize_particle(&particles[i]);
+        add_particle(&particles[i]);
+    }
 
     // Loop
     while (true)
