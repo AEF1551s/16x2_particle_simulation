4fcbf18f0ea230bb6937d92f3868bb0c75292c8e
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-07-04 18:20:58 +0300

    add description

--- include/hardware_rand.h
+++ include/hardware_rand.h
@@ -4,11 +4,15 @@
 // CMSIS
 #include <stm32f410rx.h>
 #include <stm32f4xx.h>
-//STD
+// STD
 #include <stdbool.h>
 #include <stdlib.h>
-//USER
-#include <lcd_char_disp.h> 
+// USER
+#include <lcd_char_disp.h>
+
+/*
+Generated random number using STM32F4 RNG and interrupts
+*/
 
 static bool drrdy_flag = false;
 static uint32_t rand_int = 0;
