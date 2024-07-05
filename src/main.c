8e3d87a5d446e3454b42d5ddc4529f564a006728
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-07-05 22:31:17 +0300

    Add particle simulation

--- src/main.c
+++ src/main.c
@@ -12,61 +12,52 @@
 #include <particle_char_generator.h> //PARTICLE GENERATOR FOR 16X2 CHAR DISPLAY
 #include <physics.h>                 //PARTICLES AND PHYSICS
 #include <hardware_rand.h>           //HARDWARE RNG
+#include <delay.h>                   //DELAY TIM6
 
 // All board conifigurations add here
 void initialize()
 {
-
-    // Setup
-    lcd_char_disp_init();
+    /*
+    Setup
+    Initialization functions MUST be initialized in this order.
+    For example, if delay is not initialized but its functions called, it will not perform counting and get stuck in loop
+    */
+    delay_init();
     hard_rand_init();
+    lcd_char_disp_init();
 }
 
 // Main
 int main()
 {
+    // Initialization and checks
     initialize();
     output_string((char *){"Initialization  complete"});
+    delay_ms(1000);
 
     // Add particles and randomize
-    struct particle particles[5];
-    for (int i = 0; i < 5; i++)
+    struct particle particles[3];
+    for (int i = 0; i < 3; i++)
     {
         randomize_particle(&particles[i]);
         add_particle(&particles[i]);
     }
+
     clear_display();
     output_string((char *){"Random particlesgenerated"});
+    delay_ms(1000);
 
-    // Loop
+    // END - Initialization and checks
+
     while (true)
     {
         clear_display();
-
         display_all_particles();
-        for (volatile int i = 0; i < 50000; i++) // If delay is smaller then 30000, LCD doesnt reach its maximum contrast, hard to see the particle.
-            ;
+        // LCD max response time is 200ms, typical 150, with no lower limit.
+        // Lowering the delay, LCD doesnt reach its maximum contrast and harder to see pixels
+        delay_ms(75);
         update_all_particles();
     }
     free_all_particles();
     return 0;
-}
-
-// Interrupts
-void EXTI15_10_IRQHandler(void)
-{
-    // switch (EXTI->PR)
-    // {
-    // case EXTI_PR_PR11:
-    //     break;
-
-    // default:
-    //     break;
-    // }
-    // // if interrupt happens on 11 pin
-    // if (READ_BIT(EXTI->PR, EXTI_PR_PR11))
-    // {
-    //     // Clear pending bit by writing 1 per RefManual. reset interupt request
-    //     SET_BIT(EXTI->PR, EXTI_PR_PR11);
-    // }
 }
