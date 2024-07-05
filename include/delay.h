99e6f833bd380579fe4663d9ce59f31da85ec758
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-07-05 22:28:59 +0300

    Fix comment

--- include/delay.h
+++ include/delay.h
@@ -10,10 +10,10 @@
 /*
 Implements ms delay with TIM6 (General purpose 16 bit timer) and interrupts
 Single counter cycle is 1ms. Counter counts till auto-reload then generates interupt.
-Interupts are only generated when counter reaches auto-reload value
+Interrupts are only generated when counter reaches auto-reload value
 Uses one-pulse mode - once counter has reached auto-reload value, time ir disabled until next delay_ms() call
-With default clock configuration SYSCLK and AHB AND APB are 16Mhz. 
-Delay uses TIM6 with 16000 prescaler which makes 1 interrupt around 1ms. 
+With default clock configuration SYSCLK and AHB AND APB are 16Mhz (HSI clock).
+Delay uses TIM6 with 16000 prescaler which makes 1 interrupt cycle 1ms. 
 */
 
 static bool flag = false;
